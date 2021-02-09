a76a9e7bb3e189a2dddc5f75dd98ae86d2fb6f56
jimmymaise <the@duyet.com>
2021-02-09 15:32:44 +0700

    Add comment constant

--- functions/social_networks/instagram/post_comment/constants/comment.py
+++ functions/social_networks/instagram/post_comment/constants/comment.py
@@ -10,10 +10,25 @@ class CommentConst:
                                                                             '"after":"{cursor}"}}'
     COMMENT_DATA_RESPONSE_KEY_PATH = ['data', 'shortcode_media', 'edge_media_to_parent_comment']
     COMMENT_RESOURCE_TYPE = 'COMMENT'
-
+    ACCOUNT = "account_info"
+    ACCOUNT_ID = "account_id"
+    COOKIE_FIELD = "info"
     NUM_ITEM = 'num_item'
     TIMEOUT = 'timeout'
     QUERY_HASH = 'query_hash'
     CURSOR = 'cursor'
     COOKIES = 'cookies'
     SHORTCODE = 'shortcode'
+    NUM_COUNT = "count"
+    # Comment
+    COMMENT_ID = "id"
+    COMMENT_MESS = "text"
+    COMMENT_TIME = "created_at"
+    COMMENT_LIKE = "edge_liked_by"
+    COMMENT_THREAD = "edge_threaded_comments"
+    # Commenter
+    COMMENTER_FIELD = "owner"
+    COMMENTER_ID = "id"
+    COMMENTER = "username"
+    COMMENTER_VERIFY = "is_verified"
+    COMMENTER_AVATAR = "profile_pic_url"
