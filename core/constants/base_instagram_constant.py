db5762999ea58f88a1599aabcb24ca1dee0ca221
jimmymaise <the@duyet.com>
2021-02-09 15:29:25 +0700

    Add response constant for IG post comment
    Add header to request data

--- core/constants/base_instagram_constant.py
+++ core/constants/base_instagram_constant.py
@@ -9,14 +9,6 @@ class LambdaRequestConst:
     DATA_FIELDS = 'data_fields'
 
 
-# Lambda Response
-class LambdaResponseConst:
-    STATUS_CODE = 'status_code'
-    BODY = 'body'
-    CURSOR = 'cursor'
-    MESSAGE = 'message'
-
-
 # IG Request
 class IGResponseConst:
     HAS_NEXT_PAGE = 'has_next_page'
@@ -25,6 +17,7 @@ class IGResponseConst:
     NODE = 'node'
     EDGES = 'edges'
     STATUS_CODE = 'status_code'
+    CURSOR = "cursor"
 
 
 class LambdaResponseConst:
@@ -33,10 +26,42 @@ class LambdaResponseConst:
         "data": {},
         "message": "Done"
     }
-    STATUS_CODE = "status"
     MESS_FIELD = "message"
     DESC_FIELD = "description"
     DATA_FIELD = "data"
     USER_FIELD = "user"
     POST_FIELD = "post"
     PAGING_FIELD = "paging"
+    CMT_FIELD = "comment"
+
+
+class UserResponseConst:
+    """Response constant for user info"""
+    USER_ID = "_id"
+    USERNAME = "username"
+    FULL_NAME = "full_name"
+    IS_VERIFY = "is_verify"
+    AVATAR = "avatar"
+    NUM_FOLLOWER = "num_follower"
+    NUM_FOLLOWING = "num_following"
+    NUM_PHOTO = "num_photo"
+
+
+class CmtResponseConst:
+    """Response constant for commnet info"""
+    COMMENT_ID = "_id"
+    COMMENTER_ID = "user_id"
+    COMMENTER = "username"
+    CREATED_AT = "taken_at_timestamp"
+    MESSAGE = "message"
+    NUM_LIKE = "num_like"
+    PARENT_CMT_ID = "parent_comment_id"  # If comment is "reply"
+    POST_ID = "post_id"
+
+
+DEFAULT_HEADER = {
+    "Content-Type": "application/json",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
+                  "AppleWebKit/537.36 (KHTML, like Gecko) " +
+                  "Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41"
+}
