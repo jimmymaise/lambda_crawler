c25e35a2253398cbcc70a0c4466ceca1c6e76417
jimmymaise <the@duyet.com>
2021-02-09 15:35:03 +0700

    Add comment schema

--- functions/social_networks/instagram/post_comment/schemas/comment_paging_schema.py
+++ functions/social_networks/instagram/post_comment/schemas/comment_paging_schema.py
@@ -6,9 +6,10 @@ class PagingCommentUrlOptionsSchema(Schema):
         unknown = EXCLUDE
 
     cursor = fields.Str(allow_none=True)
-    query_hash = fields.Str()
     num_item = fields.Int()
+    query_hash = fields.Str(required=True, allow_none=False)
     shortcode = fields.Str()
+    account_id = fields.Str(required=True, allow_none=False)
 
 
 class PagingCommentRequestOptionsSchema(Schema):
@@ -25,10 +26,11 @@ class AccountInfoSchema(Schema):
         unknown = EXCLUDE
 
     info = fields.Dict(required=True, allow_none=False)
-    account_if = fields.Str(required=True, allow_none=False)
+    account_id = fields.Str(required=True, allow_none=False)
+    query_hash = fields.Str(required=True, allow_none=False)
 
 
-class RequestSchema(Schema):
+class IGCommentPaginateRequestSchema(Schema):
     """Schema of request param"""
     class Meta:
         unknown = EXCLUDE
@@ -37,3 +39,48 @@ class RequestSchema(Schema):
     cursor = fields.Str(required=False, allow_none=True)
     num_item = fields.Int(required=False, allow_none=False)
     account_info = fields.Nested(AccountInfoSchema, required=True, allow_none=False)
+
+
+class CommentResponseSchema(Schema):
+    """Schema of comment param"""
+    class Meta:
+        unknown = EXCLUDE
+
+    _id = fields.Int(required=True, allow_none=False)
+    message = fields.Str(required=True, allow_none=True)
+    user_id = fields.Int(required=True, allow_none=False)
+    username = fields.Str(required=False, allow_none=True)
+    num_like = fields.Int(required=False, allow_none=True)
+    parent_comment_id = fields.Int(required=False, allow_none=True)
+    taken_at_timestamp = fields.Int(required=True, allow_none=False)
+
+
+class UserResponseSchema(Schema):
+    """Schema of user param"""
+    class Meta:
+        unknown = EXCLUDE
+
+    _id = fields.Int(required=True, allow_none=False)
+    username = fields.Str(required=True, allow_none=False)
+    avatar = fields.Str(required=True, allow_none=False)
+    is_verify = fields.Boolean(required=True, allow_none=False)
+
+
+class PostCommentSchema(Schema):
+    """Schema for post comment response"""
+    class Meta:
+        unknown = EXCLUDE
+
+    user = fields.Nested(UserResponseSchema, required=True)
+    comment = fields.Nested(CommentResponseSchema, required=True)
+
+
+class IGPostCommentResponseSchema(Schema):
+    """Schema of response data to API Gateway"""
+    class Meta:
+        unknown = EXCLUDE
+
+    data = fields.Nested(PostCommentSchema, required=False)
+    paging = fields.Dict()
+    message = fields.Str()
+    description = fields.Str()
